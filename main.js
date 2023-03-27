29acdfbcf18b3e7503cbd74a88fc234326b84d6a
JanGospodarek <jan.gospo@onet.pl>
2023-03-27 13:25:12 +0200

    moving conrolsgit add -A! :))

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var Game = /** @class */ (function () {
     function Game() {
-        this.boardCon = document.getElementById('board');
+        this.boardCon = document.getElementById("board");
         this.renderBoard();
         this.renderPionek();
     }
     Game.prototype.renderBoard = function () {
         for (var y = 0; y < 15; y++) {
             for (var x = 0; x < 8; x++) {
-                var div = document.createElement('div');
-                div.classList.add('cell');
+                var div = document.createElement("div");
+                div.classList.add("cell");
                 div.style.left = "".concat(20 * x, "px");
                 div.style.top = "".concat(20 * y, "px");
                 this.boardCon.append(div);
@@ -22,32 +22,80 @@ var Game = /** @class */ (function () {
 }());
 var Pionek = /** @class */ (function () {
     function Pionek(boardDiv) {
+        this.manualMovingDown = false;
         this.boardDiv = boardDiv;
         this.buildPionek();
         this.moving();
+        this.addControls();
     }
     Pionek.prototype.buildPionek = function () {
-        this.pionek = document.createElement('div');
-        var cell1 = document.createElement('div');
-        var cell2 = document.createElement('div');
-        cell1.style.backgroundColor = 'red';
-        cell2.style.backgroundColor = 'blue';
-        this.pionek.classList.add('pionek');
-        cell1.classList.add('pionek-cell');
-        cell2.classList.add('pionek-cell');
+        this.pionek = document.createElement("div");
+        var cell1 = document.createElement("div");
+        var cell2 = document.createElement("div");
+        cell1.style.backgroundColor = "red";
+        cell2.style.backgroundColor = "blue";
+        this.pionek.classList.add("pionek");
+        cell1.classList.add("pionek-cell");
+        cell2.classList.add("pionek-cell");
         this.pionek.append(cell1);
         this.pionek.append(cell2);
-        this.pionek.style.left = '60px';
+        this.pionek.style.left = "60px";
         this.boardDiv.append(this.pionek);
     };
     Pionek.prototype.moving = function () {
         var _this = this;
-        var i = 0;
+        // let i = +this.pionek.style.top.split("p")[0] / 20;
         this.movingInterval = setInterval(function () {
-            console.log(20 * _this.movingInterval);
-            _this.pionek.style.top = "".concat(20 * i, "px");
-            i++;
+            if (_this.manualMovingDown)
+                return;
+            var y = 20 + Number(_this.pionek.style.top.split("p")[0]);
+            var x = +_this.pionek.style.left.split("p")[0];
+            if (_this.chechForBorderCollisions(x, y, true)) {
+                clearInterval(_this.movingInterval);
+                return;
+            }
+            _this.pionek.style.top = "".concat(y, "px");
+            //   i++;
         }, 400);
+    };
+    Pionek.prototype.chechForBorderCollisions = function (x, y, autonomous) {
+        console.log(x, y);
+        if (autonomous) {
+            if (y >= 300)
+                return true;
+        }
+        else {
+            if (x <= 0 || x >= 160 || y >= 300)
+                return true;
+        }
+    };
+    Pionek.prototype.addControls = function () {
+        var _this = this;
+        document.addEventListener("keydown", function (e) {
+            var key = e.key;
+            var x = +_this.pionek.style.left.split("p")[0];
+            var y = +_this.pionek.style.top.split("p")[0];
+            switch (key) {
+                case "ArrowLeft":
+                    if (!_this.chechForBorderCollisions(x, y, false))
+                        _this.pionek.style.left = "".concat(x - 20, "px");
+                    break;
+                case "ArrowRight":
+                    if (!_this.chechForBorderCollisions(x + 40, y, false))
+                        _this.pionek.style.left = "".concat(x + 20, "px");
+                    break;
+                case "ArrowDown":
+                    if (!_this.chechForBorderCollisions(x, y + 20, false)) {
+                        _this.manualMovingDown = true;
+                        _this.pionek.style.top = "".concat(y + 20, "px");
+                    }
+                    break;
+            }
+        });
+        document.addEventListener("keyup", function (e) {
+            if (e.key == "ArrowDown")
+                _this.manualMovingDown = false;
+        });
     };
     return Pionek;
 }());
--- main.ts
+++ main.ts
@@ -1,60 +1,106 @@
-class Game{
-    private boardCon=<HTMLDivElement>document.getElementById('board')
-    constructor(){
-        this.renderBoard()
-        this.renderPionek()
+class Game {
+  private boardCon = <HTMLDivElement>document.getElementById("board");
+  constructor() {
+    this.renderBoard();
+    this.renderPionek();
+  }
+  private renderBoard() {
+    for (let y = 0; y < 15; y++) {
+      for (let x = 0; x < 8; x++) {
+        const div = document.createElement("div");
+        div.classList.add("cell");
+        div.style.left = `${20 * x}px`;
+        div.style.top = `${20 * y}px`;
+        this.boardCon.append(div);
+      }
     }
-    private renderBoard(){
-        
-        for (let y = 0; y < 15; y++) {
-            for (let x = 0; x < 8; x++) {
-                const div=document.createElement('div')
-                div.classList.add('cell')
-                div.style.left=`${20*x}px`
-                div.style.top=`${20*y}px`
-                this.boardCon.append(div)
-            }
-            
-        }
-    }
-    renderPionek(){
-        const pionek=new Pionek(this.boardCon)
-    }
-
-
+  }
+  private renderPionek() {
+    const pionek = new Pionek(this.boardCon);
+  }
 }
-class Pionek{
-    boardDiv:HTMLDivElement
-    pionek:HTMLDivElement
-    movingInterval:any
-    constructor(boardDiv:HTMLDivElement){
-        this.boardDiv=boardDiv
-        this.buildPionek()
-        this.moving()
+
+class Pionek {
+  private boardDiv: HTMLDivElement;
+  private pionek: HTMLDivElement;
+  private movingInterval: any;
+  private manualMovingDown = false;
+  constructor(boardDiv: HTMLDivElement) {
+    this.boardDiv = boardDiv;
+    this.buildPionek();
+    this.moving();
+    this.addControls();
+  }
+
+  private buildPionek() {
+    this.pionek = document.createElement("div");
+    const cell1 = document.createElement("div");
+    const cell2 = document.createElement("div");
+    cell1.style.backgroundColor = "red";
+    cell2.style.backgroundColor = "blue";
+    this.pionek.classList.add("pionek");
+    cell1.classList.add("pionek-cell");
+    cell2.classList.add("pionek-cell");
+    this.pionek.append(cell1);
+    this.pionek.append(cell2);
+    this.pionek.style.left = "60px";
+    this.boardDiv.append(this.pionek);
+  }
+
+  private moving() {
+    // let i = +this.pionek.style.top.split("p")[0] / 20;
+    this.movingInterval = setInterval(() => {
+      if (this.manualMovingDown) return;
+
+      const y = 20 + Number(this.pionek.style.top.split("p")[0]);
+      const x = +this.pionek.style.left.split("p")[0];
+
+      if (this.chechForBorderCollisions(x, y, true)) {
+        clearInterval(this.movingInterval);
+        return;
+      }
+
+      this.pionek.style.top = `${y}px`;
+      //   i++;
+    }, 400);
+  }
+
+  private chechForBorderCollisions(x: number, y: number, autonomous: boolean) {
+    console.log(x, y);
+    if (autonomous) {
+      if (y >= 300) return true;
+    } else {
+      if (x <= 0 || x >= 160 || y >= 300) return true;
     }
-    buildPionek(){
-        this.pionek=document.createElement('div')
-        const cell1=document.createElement('div')
-        const cell2=document.createElement('div')
-        cell1.style.backgroundColor='red'
-        cell2.style.backgroundColor='blue'
-        this.pionek.classList.add('pionek')
-        cell1.classList.add('pionek-cell')
-        cell2.classList.add('pionek-cell')
-        this.pionek.append(cell1)
-        this.pionek.append(cell2)
-        this.pionek.style.left='60px'
-        this.boardDiv.append(this.pionek)
-        
-    }
-    moving(){
-        let i=0
-       this.movingInterval= setInterval(()=>{
-        console.log(20*this.movingInterval);
-        
-        this.pionek.style.top=`${20*i}px`
-        i++
-       },400)
-    }
+  }
+  private addControls() {
+    document.addEventListener("keydown", (e: KeyboardEvent) => {
+      const key = e.key;
+      const x = +this.pionek.style.left.split("p")[0];
+      const y = +this.pionek.style.top.split("p")[0];
+
+      switch (key) {
+        case "ArrowLeft":
+          if (!this.chechForBorderCollisions(x, y, false))
+            this.pionek.style.left = `${x - 20}px`;
+          break;
+        case "ArrowRight":
+          if (!this.chechForBorderCollisions(x + 40, y, false))
+            this.pionek.style.left = `${x + 20}px`;
+
+          break;
+        case "ArrowDown":
+          if (!this.chechForBorderCollisions(x, y + 20, false)) {
+            this.manualMovingDown = true;
+            this.pionek.style.top = `${y + 20}px`;
+          }
+
+          break;
+      }
+    });
+    document.addEventListener("keyup", (e: KeyboardEvent) => {
+      if (e.key == "ArrowDown") this.manualMovingDown = false;
+    });
+  }
 }
-const game=new Game()
+const game = new Game();
