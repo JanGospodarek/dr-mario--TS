635d53118850199c92611b0701ed6539f79c143d
JanGospodarek <jan.gospo@onet.pl>
2023-03-30 09:21:32 +0200

    coloring

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 var Game = /** @class */ (function () {
     function Game() {
+        var _this = this;
         this.boardCon = document.getElementById("board");
+        this.pionks = [];
+        this.renew = function () {
+            _this.renderPionek();
+        };
+        this.checkBorderPionks = function (pos) {
+            console.log(pos);
+            var xLeft = pos.x - 20;
+            var xRight = pos.x + 20;
+            var y = pos.y + 20;
+            // const index=this.pionks.findIndex(el=>(el.position.x==xLeft||el.position.x==xRight)&&el.position.y==y)
+            var index = null;
+            _this.pionks.forEach(function (el) {
+                if (el.position.x == xLeft || el.position.x == xLeft && el.position.y == y) {
+                }
+            });
+            if (index != null)
+                return false;
+            else
+                return true;
+        };
         this.renderBoard();
         this.renderPionek();
     }
@@ -9,85 +30,118 @@ var Game = /** @class */ (function () {
             for (var x = 0; x < 8; x++) {
                 var div = document.createElement("div");
                 div.classList.add("cell");
-                div.style.left = "".concat(20 * x, "px");
-                div.style.top = "".concat(20 * y, "px");
+                div.style.left = 20 * x + "px";
+                div.style.top = 20 * y + "px";
                 this.boardCon.append(div);
             }
         }
     };
     Game.prototype.renderPionek = function () {
-        var pionek = new Pionek(this.boardCon);
+        var pionek = new Pionek(this.boardCon, this.renew, this.checkBorderPionks);
+        this.pionks.push(pionek);
     };
     return Game;
 }());
 var Pionek = /** @class */ (function () {
-    function Pionek(boardDiv) {
+    function Pionek(boardDiv, renew, checkBorderPionks) {
+        this.possibleColors = ["#BB8FCE", "#85C1E9", "#F7DC6F", "#F1948A", "#E59866"];
+        this.stop = false;
         this.manualMovingDown = false;
+        ///
+        this.position = { x: 60, y: 0 };
         this.boardDiv = boardDiv;
         this.buildPionek();
         this.moving();
         this.addControls();
+        this.renewGame = renew;
+        this.checkBorderPionks = checkBorderPionks;
     }
+    Pionek.prototype.getColor = function (except) {
+        var i = Math.floor(Math.random() * 5);
+        while (i == except) {
+            i = Math.floor(Math.random() * 5);
+        }
+        return i;
+    };
     Pionek.prototype.buildPionek = function () {
         this.pionek = document.createElement("div");
-        var cell1 = document.createElement("div");
-        var cell2 = document.createElement("div");
-        cell1.style.backgroundColor = "red";
-        cell2.style.backgroundColor = "blue";
         this.pionek.classList.add("pionek");
-        cell1.classList.add("pionek-cell");
-        cell2.classList.add("pionek-cell");
-        this.pionek.append(cell1);
-        this.pionek.append(cell2);
+        var fristColor = null;
+        for (var index = 0; index < 2; index++) {
+            var cell1 = document.createElement("div");
+            var colorIndex = this.getColor(fristColor);
+            fristColor = colorIndex;
+            cell1.style.backgroundColor = this.possibleColors[colorIndex];
+            cell1.classList.add("pionek-cell");
+            this.pionek.append(cell1);
+        }
         this.pionek.style.left = "60px";
         this.boardDiv.append(this.pionek);
     };
     Pionek.prototype.moving = function () {
         var _this = this;
-        // let i = +this.pionek.style.top.split("p")[0] / 20;
         this.movingInterval = setInterval(function () {
             if (_this.manualMovingDown)
                 return;
             var y = 20 + Number(_this.pionek.style.top.split("p")[0]);
-            var x = +_this.pionek.style.left.split("p")[0];
-            if (_this.chechForBorderCollisions(x, y, true)) {
+            if (_this.chechForBorderCollisions(undefined, y, true)) {
                 clearInterval(_this.movingInterval);
+                _this.renewGame();
                 return;
             }
-            _this.pionek.style.top = "".concat(y, "px");
-            //   i++;
+            _this.position.y = y;
+            _this.pionek.style.top = y + "px";
         }, 400);
     };
     Pionek.prototype.chechForBorderCollisions = function (x, y, autonomous) {
-        console.log(x, y);
+        if (this.checkBorderPionks(this.position)) {
+            console.log('PIONEK!!!!');
+        }
         if (autonomous) {
-            if (y >= 300)
+            if (y >= 300) {
+                this.stop = true;
                 return true;
+            }
         }
         else {
-            if (x <= 0 || x >= 160 || y >= 300)
+            if (x <= 0 || x >= 160) {
                 return true;
+            }
+            else if (y >= 300) {
+                this.stop = true;
+                return true;
+            }
         }
     };
     Pionek.prototype.addControls = function () {
         var _this = this;
         document.addEventListener("keydown", function (e) {
+            if (_this.stop)
+                return;
             var key = e.key;
             var x = +_this.pionek.style.left.split("p")[0];
             var y = +_this.pionek.style.top.split("p")[0];
             switch (key) {
                 case "ArrowLeft":
-                    if (!_this.chechForBorderCollisions(x, y, false))
-                        _this.pionek.style.left = "".concat(x - 20, "px");
+                    if (!_this.chechForBorderCollisions(x, y, false)) {
+                        var span = x - 20;
+                        _this.pionek.style.left = span + "px";
+                        _this.position.x = span;
+                    }
                     break;
                 case "ArrowRight":
-                    if (!_this.chechForBorderCollisions(x + 40, y, false))
-                        _this.pionek.style.left = "".concat(x + 20, "px");
+                    if (!_this.chechForBorderCollisions(x + 40, y, false)) {
+                        var span = x + 20;
+                        _this.pionek.style.left = span + "px";
+                        _this.position.x = span;
+                    }
                     break;
                 case "ArrowDown":
                     if (!_this.chechForBorderCollisions(x, y + 20, false)) {
                         _this.manualMovingDown = true;
-                        _this.pionek.style.top = "".concat(y + 20, "px");
+                        var span = y + 20;
+                        _this.pionek.style.top = span + "px";
+                        _this.position.y = span;
                     }
                     break;
             }
--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
+
 class Game {
   private boardCon = <HTMLDivElement>document.getElementById("board");
+  public pionks:Pionek[]=[]
   constructor() {
     this.renderBoard();
     this.renderPionek();
@@ -15,84 +17,150 @@ class Game {
       }
     }
   }
+  public renew=()=>{
+    this.renderPionek()
+  }
+  public checkBorderPionks=(pos:{x:number,y:number}):boolean=>{
+    console.log(pos);
+    const xLeft=pos.x-20
+    const xRight=pos.x+20
+    const y=pos.y+20
+    // const index=this.pionks.findIndex(el=>(el.position.x==xLeft||el.position.x==xRight)&&el.position.y==y)
+    let index:number|null=null
+    this.pionks.forEach(el=>{
+  if(el.position.x==xLeft||el.position.x==xLeft&&el.position.y==y){
+
+  }
+})
+    if(index!=null) return false
+    else return true  
+  }
   private renderPionek() {
-    const pionek = new Pionek(this.boardCon);
+    const pionek = new Pionek(this.boardCon,this.renew,this.checkBorderPionks);
+    this.pionks.push(pionek)
   }
 }
 
 class Pionek {
   private boardDiv: HTMLDivElement;
-  private pionek: HTMLDivElement;
+  private pionek!: HTMLDivElement;
   private movingInterval: any;
+  private possibleColors=["#BB8FCE","#85C1E9","#F7DC6F","#F1948A","#E59866"]
+  private stop=false;
+  private checkBorderPionks:Function;
+  private renewGame:Function;
   private manualMovingDown = false;
-  constructor(boardDiv: HTMLDivElement) {
+  ///
+  public position={x:60,y:0};
+  constructor(boardDiv: HTMLDivElement,renew:Function,checkBorderPionks:Function) {
     this.boardDiv = boardDiv;
     this.buildPionek();
     this.moving();
     this.addControls();
+    this.renewGame=renew
+    this.checkBorderPionks=checkBorderPionks
   }
+private getColor(except:number|null){
+  let i=Math.floor(Math.random()*5)
 
+  while(i==except){
+i=Math.floor(Math.random()*5)
+  }
+return i
+}
   private buildPionek() {
+    
     this.pionek = document.createElement("div");
+        this.pionek.classList.add("pionek");
+    let fristColor:number|null=null;
+
+    for (let index = 0; index<2; index++) {
     const cell1 = document.createElement("div");
-    const cell2 = document.createElement("div");
-    cell1.style.backgroundColor = "red";
-    cell2.style.backgroundColor = "blue";
-    this.pionek.classList.add("pionek");
+
+    const colorIndex=this.getColor(fristColor);
+    fristColor=colorIndex
+    cell1.style.backgroundColor = this.possibleColors[colorIndex]
+
     cell1.classList.add("pionek-cell");
-    cell2.classList.add("pionek-cell");
     this.pionek.append(cell1);
-    this.pionek.append(cell2);
+
+      
+    }
+ 
     this.pionek.style.left = "60px";
     this.boardDiv.append(this.pionek);
   }
 
   private moving() {
-    // let i = +this.pionek.style.top.split("p")[0] / 20;
     this.movingInterval = setInterval(() => {
       if (this.manualMovingDown) return;
 
       const y = 20 + Number(this.pionek.style.top.split("p")[0]);
-      const x = +this.pionek.style.left.split("p")[0];
 
-      if (this.chechForBorderCollisions(x, y, true)) {
-        clearInterval(this.movingInterval);
+      if (this.chechForBorderCollisions(undefined, y, true)) {
+        clearInterval(this.movingInterval)
+        this.renewGame();
         return;
       }
-
+      this.position.y=y
       this.pionek.style.top = `${y}px`;
-      //   i++;
     }, 400);
   }
 
-  private chechForBorderCollisions(x: number, y: number, autonomous: boolean) {
-    console.log(x, y);
+
+  private chechForBorderCollisions(x: any, y: number, autonomous: boolean) {
+    if(this.checkBorderPionks(this.position)){
+      console.log('PIONEK!!!!');
+      
+    }
+
     if (autonomous) {
-      if (y >= 300) return true;
+      if (y >= 300) {
+        this.stop=true
+        return true;
+      }
     } else {
-      if (x <= 0 || x >= 160 || y >= 300) return true;
+      if (x <= 0 || x >= 160 ) {
+        return true;
+
+      }else if( y >= 300){
+        this.stop=true;
+        return true;
+
+      }
     }
   }
   private addControls() {
     document.addEventListener("keydown", (e: KeyboardEvent) => {
+      if(this.stop) return;
       const key = e.key;
       const x = +this.pionek.style.left.split("p")[0];
       const y = +this.pionek.style.top.split("p")[0];
 
       switch (key) {
         case "ArrowLeft":
-          if (!this.chechForBorderCollisions(x, y, false))
-            this.pionek.style.left = `${x - 20}px`;
+          if (!this.chechForBorderCollisions(x, y, false)){
+              const span=x - 20
+            this.pionek.style.left = `${span}px`;
+            this.position.x=span
+          }
+
           break;
         case "ArrowRight":
-          if (!this.chechForBorderCollisions(x + 40, y, false))
-            this.pionek.style.left = `${x + 20}px`;
+          if (!this.chechForBorderCollisions(x + 40, y, false)){
+              const span=x + 20
+            this.pionek.style.left = `${span}px`;
+            this.position.x=span
+          }
 
           break;
         case "ArrowDown":
           if (!this.chechForBorderCollisions(x, y + 20, false)) {
             this.manualMovingDown = true;
-            this.pionek.style.top = `${y + 20}px`;
+            const span=y+20
+            this.pionek.style.top = `${span}px`;
+            this.position.y=span
+          
           }
 
           break;
